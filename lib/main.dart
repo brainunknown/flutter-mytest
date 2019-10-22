3ade565b39f46652fc436fd0208a2c04cef5ce3c
RFR clevo <engineer.ricardoribeiro@gmail.com>
2019-10-22 23:01:08 +0100

    initial

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_ui_redesign/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Netflix UI Redesign',
+      debugShowCheckedModeBanner: false,
+      home: PageView(
+          children: <Widget>[
+            Container(
+
+              color: Colors.pink,
+              child: HomeScreen(),
+            ),
+            Container(
+              color: Colors.cyan,
+              child: photoGallery(),
+            ),
+            Container(
+              color: Colors.deepPurple,
+            ),
+
+          ],
+        scrollDirection: Axis.vertical,
+        onPageChanged: (num) {
+          print("Número da página atual : " + num.toString());
+        },
+      ),
+
+
+    );
+    //Container(child: HomeScreen(),
+    //   Container(child: photoGallery(),
+
+
+  }
+}
+
+
+ photoGallery (){
+  return PageView(
+    scrollDirection: Axis.vertical,
+    children: <Widget>[
+      Container(
+        color: Colors.amber,
+      ),
+      Container(
+        color: Colors.blueAccent,
+      ),
+      Container(
+        color: Colors.pinkAccent,
+      ),
+    ],
+  );
+}
